9bf6c3a1c9c526431751536719a5c3012f5d91f2
rossspencer715 <rossspencer715@gmail.com>
2018-09-17 21:10:13 -0400

    updated config and JSONtoMongo to get my project working with my Mlab database

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -3,28 +3,50 @@
   Import modules/files you may need to correctly run the script. 
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
-var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
-    config = require('./config');
-    json = require('./listings.json');
 
+var fs = require('fs'),
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config');
 
 /* Connect to your database */
-mongoose.connect(config.db.uri)
+<<<<<<< HEAD
+mongoose.connect(config.db.uri);
+var connection = mongoose.connection;
+connection.on("connected", function(){
+  console.log("Database is connected!");
+});
+/* Instantiate a mongoose model for each listing object in the JSON file, 
+  and then save it to your Mongo database */
+ 
+fs.readFile('listings.json','utf8',function(err,data){
+  if(err){
+    throw err;
+  }
 
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
- */
-Listing.insertMany(json.entries, function(err,result){
-    if(err) 
-      throw err;
-    mongoose.disconnect();
+  var listings = JSON.parse(data).entries;
+  var size = listings.length;
+
+  for(var k = 0; k < size; k++){
+    var model = new Listing(listings[k]);
+    model.save(function(err){
+      if(err){
+        throw err;
+      }
+      else{
+        console.log("Listing added to our database!");
+      }
+    });
+  }
+
+=======
+mongoose.connect(config.db.uri)
+>>>>>>> 05b2d24b6cb4e79cd5ee63c7a7d42d9919265cfc
+
 });
 
-/* 
+/*
   Once you've written + run the script, check out your MongoLab database to ensure that 
   it saved everything correctly. 
  */
